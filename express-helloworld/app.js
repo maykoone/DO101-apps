d7027a5d5d579ac500eec374504ec9a26317b97f
Mayko B. Oliveira <maykoone@gmail.com>
2020-05-05 23:00:45 -0300

    added quote of the endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,9 +1,20 @@
 var express = require('express');
 app = express();
 
+var quotes = [
+  'The supernatural is the natural not yet understood.',
+  'Even death is not to be feared by one who has lived wisely.',
+  'Each time you love, love as deeply as if it were forever.'
+]
+
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
+
+app.get('/quotes', function(req, res) {
+  var randomNumber = Math.floor(Math.random() * quotes.length);
+  res.send(quotes[randomNumber]);
+})
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
